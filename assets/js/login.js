2663edf8d22670ddb652f064d2d8b947df02386c
wangze <wangze@qq.com>
2020-09-10 11:59:57 +0800

    写了点

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,5 +7,20 @@ $(function () {
         $('.login-box').show()
         $('.reg-box').hide()
     })
+    // 从layui里获取form
+    var form = layui.form
+    // layui自定义验证
+    form.verify({
+        pass: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
+    })
+    // 监听注册表单提交事件
+    $('#form_reg').on('submit', function () {
 
+    })
 })
