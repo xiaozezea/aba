d46866e2ecb388ed4f2832b1d4eb0c5fc621b612
wangze <wangze@qq.com>
2020-09-10 15:57:39 +0800

    登录注册写完了

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,6 +9,7 @@ $(function () {
     })
     // 从layui里获取form
     var form = layui.form
+    var layer = layui.layer
     // layui自定义验证
     form.verify({
         pass: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
@@ -20,7 +21,35 @@ $(function () {
         }
     })
     // 监听注册表单提交事件
-    $('#form_reg').on('submit', function () {
-
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form_reg [name=title]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功,请登录')
+            $('#link_login').click()
+        })
+    })
+    $('#form_login').submit(function () {
+        $.ajax({
+            method: 'POST',
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
     })
 })
