59c4bbc883dd7944dd35d302ea856d94cf141cd6
wangze <wangze@qq.com>
2020-09-12 16:03:04 +0800

    头像差不多了

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,47 @@
+$(function () {
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '认真输入啊,少于六位'
+            }
+        }
+    })
+
+    initUserInfo()
+    function initUserInfo() {
+        $.ajax({
+            method: 'get',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取失败')
+                }
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+    $('#btnReset').on('click', function (e) {
+        e.preventDefault()
+        initUserInfo()
+    })
+    $('.layui-form').on('submit', function (e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault()
+        // 发起 ajax 数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功！')
+                // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+                window.parent.grtUserInfo()
+            }
+        })
+    })
+})
